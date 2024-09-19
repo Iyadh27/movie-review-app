7fbbf06e629f45dde95f86ce0828b009f3af2044
Mohamed Iyadh Khan <iyadh.21@cse.mrt.ac.lk>
2024-09-20 01:03:19 +0530

    created ReviewService

---
+++ src/main/java/dev/iyadh/movies/ReviewController.java
@@ -0,0 +1,17 @@
+package dev.iyadh.movies;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    @Service
+    public ReviewService service ;
+
+    @PostMapping
+    public
+}
